4d96e34e9e15909222550564fe8de42633130d42
Hosana Joshua <64140220+Joshua-254@users.noreply.github.com>
2023-06-16 22:34:21 +0300

    Add files via upload

---
+++ inpatient.php
@@ -0,0 +1,26 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "hospital";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "INSERT INTO in_patient (firstname, lastname, location, age, telephone, email, dob)
+VALUES ('$_POST[fname]','$_POST[lname]','$_POST[location]','$_POST[age]','$_POST[telephone]',
+'$_POST[email]', '$_POST[dob]' )";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record Added successfully";
+  header("Refresh:3; url=index.html");
+} else {
+  echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
